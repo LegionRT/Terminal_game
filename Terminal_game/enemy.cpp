35bd9b267c096a91d304d4a1e6ace82561ed2d86
LegionRT <ivanivanaripov@gmail.com>
2026-06-11 15:21:23 +0500

    Delete Terminal_game directory

--- Terminal_game/enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "enemy.h"
-#include <iostream>
-
-void Enemy::drop_loot(Inventory& inv) {
-    if (!inventory.is_empty()) {
-        std::cout << "The enemy dropped loot:\n";
-        inventory.transfer_all_to(inv);
-    }
-}
